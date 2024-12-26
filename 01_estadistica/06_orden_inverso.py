984e60b0037283eb0b05eea2d140217ac5dd23ee
Felipe Zambrano <felipe.zambrano@gmail.com>
2024-12-26 18:53:12 -0300

    Se mueve archivos a la carpeta 01_estadistica

--- 01_estadistica/06_orden_inverso.py
+++ 01_estadistica/06_orden_inverso.py
@@ -5,5 +5,7 @@ def invertir(N):
     unidades = N % 10
     decenas = N // 10
     return unidades * 10 + decenas
+
+#tests
 assert invertir(28) == 82 #prueba de caso nominal
 assert invertir(33) == 33 #prueba de caso borde o caso limite
