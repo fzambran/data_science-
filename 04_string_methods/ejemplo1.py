7211ecdf7191f474f3b29f5e6b3919ca8ecb2c29
Felipe Zambrano <fzambran@gmail.com>
2024-12-30 16:48:04 -0300

    Se suben ejemplos de string methods

---
+++ 04_string_methods/ejemplo1.py
@@ -0,0 +1,14 @@
+print("temperatures and facts about the moon".title())
+
+heading = "\ntemperatures and facts about the moon"
+heading_upper = heading.title()
+print(heading_upper)
+
+temperatures = "Daylight: 260 F Nighttime: -280 F"
+temperatures_list = temperatures.split()
+print(temperatures_list)
+
+temperatures = "Daylight: 260 F\n Nighttime: -280 F"
+temperatures_list = temperatures.split('\n')
+print(temperatures_list)
+
